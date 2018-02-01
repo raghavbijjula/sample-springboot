882d517764c9d0e0322ca9ebe555a2d2bb6afa77
raghavbijjula <35055214+raghavbijjula@users.noreply.github.com>
2018-02-01 23:06:56 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello IDT Hyderabad!";
+        return "This is SpringBoot Demo!";
     }
     
 }
